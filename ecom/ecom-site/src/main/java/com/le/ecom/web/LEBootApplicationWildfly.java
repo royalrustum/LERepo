b7a539a9a17c07c8c85a04f999abbcef95b8e225
Kiran <royalrustum@gmail.com>
2016-09-29 23:17:41 -0400

    Adds seperate SpringBootApplication classes for Tomcat and Wildfly

--- ecom/ecom-site/src/main/java/com/le/ecom/web/LEBootApplicationWildfly.java
+++ ecom/ecom-site/src/main/java/com/le/ecom/web/LEBootApplicationWildfly.java
@@ -19,20 +19,24 @@ package com.le.ecom.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@ComponentScan
-/*@Configuration
+/*@ComponentScan
+@Configuration
 @EnableWebMvc
 @EnableAutoConfiguration*/
+@ComponentScan
 @SpringBootApplication
-public class LEBootApplication   {
+@Profile("wildfly")
+public class LEBootApplicationWildfly extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(LEBootApplication.class, args);
+	public static void main(String args[]) {
+		SpringApplication.run(LEBootApplicationWildfly.class, args);
 	}
 
 }
